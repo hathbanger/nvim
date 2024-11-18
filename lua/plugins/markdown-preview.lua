1c65eed20543d5d0ee700989289522c58c70a888
hathbanger <hathbanger.ah@gmail.com>
2024-11-18 06:45:41 -0700

    Update plugins: switch from peek.nvim to markdown-preview.nvim and update lazy-lock.json

--- lua/plugins/markdown-preview.lua
+++ lua/plugins/markdown-preview.lua
@@ -1,10 +1,9 @@
 return {
-  "toppair/peek.nvim",
-  event = { "VeryLazy" },
-  build = "deno task --quiet build:fast",
-  config = function()
-    require("peek").setup()
-    vim.api.nvim_create_user_command("PeekOpen", require("peek").open, {})
-    vim.api.nvim_create_user_command("PeekClose", require("peek").close, {})
+  "iamcco/markdown-preview.nvim",
+  cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
+  build = "cd app && yarn install",
+  init = function()
+    vim.g.mkdp_filetypes = { "markdown" }
   end,
+  ft = { "markdown" },
 }
